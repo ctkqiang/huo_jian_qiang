291199b3e360740480ac5bd89891870bbbb8a1bf
🇨🇳钟智强 『江西青垣科技』 <johnmelodymel@qq.com>
2025-12-19 11:17:39 +0800

    refactor(http): 使用url.Parse处理GET请求的URL参数
    重构GET函数，使用url.Parse解析URI并处理URL参数
    修复日志中URL显示问题，统一使用basedURL

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"huo_jian_qiang/internal/model"
 	"huo_jian_qiang/pkg/logger"
@@ -12,8 +13,12 @@ import (
 var log = logger.New("debug")
 var userRequest model.UserRequest
 
-func GET(url string, userRequest model.UserRequest) error {
-	resp, err := http.Get(url)
+func GET(uri string, userRequest model.UserRequest) error {
+
+	basedURL, err := url.Parse(uri)
+
+	param := url.Values{}
+	resp, err := http.Get(basedURL.String() + "?" + param.Encode())
 
 	if err != nil {
 		return err
@@ -22,17 +27,18 @@ func GET(url string, userRequest model.UserRequest) error {
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
+
 	if err != nil {
 		panic(err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		log.Error("请求GET %s 失败，状态码: %d", url, resp.StatusCode)
+		log.Error("请求GET %s 失败，状态码: %d", basedURL, resp.StatusCode)
 
-		return fmt.Errorf("GET %s 返回非200状态码: %d |", url, resp.StatusCode)
+		return fmt.Errorf("GET %s 返回非200状态码: %d |", basedURL, resp.StatusCode)
 	}
 
-	log.Info("请求GET %s 成功，状态码: %d | %s", url, body, resp.StatusCode)
+	log.Info("请求GET %s 成功，状态码: %d | %s", basedURL, body, resp.StatusCode)
 
 	return nil
 }
